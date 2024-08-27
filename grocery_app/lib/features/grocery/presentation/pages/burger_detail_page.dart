561dd9d595b5af3414c25c23cab5c4837c9e7ab7
Kalki767 <kalkidankidane3@gmail.com>
2024-08-27 17:35:31 +0300

    Update the detail page

--- grocery_app/lib/features/grocery/presentation/pages/burger_detail_page.dart
+++ grocery_app/lib/features/grocery/presentation/pages/burger_detail_page.dart
@@ -11,7 +11,7 @@ class BurgerDetailPage extends StatelessWidget {
         children: [
           // Main Content with Image and Details
           SingleChildScrollView(
-            padding: EdgeInsets.all(16),
+            //
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
@@ -22,104 +22,115 @@ class BurgerDetailPage extends StatelessWidget {
                         BorderRadius.vertical(bottom: Radius.circular(12)),
                     child: Image.network(
                       args.imageUrl,
-                      width: double.infinity,
+                      width: MediaQuery.of(context).size.width, // Full width
                       height: 250,
                       fit: BoxFit.cover,
                     ),
                   ),
                 ),
-                SizedBox(height: 16),
-                Text(
-                  args.title,
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 8),
-                Row(
-                  children: [
-                    Text(
-                      '\$${args.discount}',
-                      style: TextStyle(
-                        decoration: TextDecoration.lineThrough,
-                        color: Colors.grey,
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 24),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 16),
+                      Text(
+                        args.title,
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
                       ),
-                    ),
-                    SizedBox(width: 8),
-                    Text(
-                      '\$${args.price}',
-                      style: TextStyle(
-                        color: Colors.red,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
+                      SizedBox(height: 8),
+                      Row(
+                        children: [
+                          Text(
+                            '\$${args.discount}',
+                            style: TextStyle(
+                              decoration: TextDecoration.lineThrough,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          SizedBox(width: 8),
+                          Text(
+                            '\$${args.price}',
+                            style: TextStyle(
+                              color: Colors.red,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 8),
-                Row(
-                  children: [
-                    Icon(Icons.star, color: Colors.amber, size: 24),
-                    SizedBox(width: 4),
-                    Text('${args.rating}', style: TextStyle(fontSize: 16)),
-                    SizedBox(width: 4),
-                    Text('(1,205)', style: TextStyle(color: Colors.grey)),
-                    Spacer(),
-                    Text('See all reviews',
-                        style: TextStyle(color: Colors.blue)),
-                  ],
-                ),
-                SizedBox(height: 16),
-                Text(
-                  'A delicious chicken burger served on a toasted bun with fresh lettuce, tomato slices, and mayonnaise. Juicy grilled chicken patty seasoned to perfection.',
-                  style: TextStyle(fontSize: 16, color: Colors.grey),
-                ),
-                SizedBox(height: 16),
-                Text(
-                  'Additional Options:',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 8),
-                Column(
-                  children: [
-                    ListView.builder(
-                      shrinkWrap: true, // Adjust size based on content
-                      physics:
-                          NeverScrollableScrollPhysics(), // Disable internal scrolling
-                      itemCount: args.options.length,
-                      itemBuilder: (context, index) {
-                        return OptionItem(
-                          title: args.options[index].name,
-                          price: '+ £${args.options[index].price}',
-                        );
-                      },
-                    ),
-                  ],
-                ),
-                SizedBox(height: 16),
-                Row(
-                  children: [
-                    IconButton(
-                      icon: Icon(Icons.remove),
-                      onPressed: () {},
-                    ),
-                    Text('1', style: TextStyle(fontSize: 18)),
-                    IconButton(
-                      icon: Icon(Icons.add),
-                      onPressed: () {},
-                    ),
-                    Spacer(),
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 50, vertical: 12),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        backgroundColor: Colors.orange,
+                      SizedBox(height: 8),
+                      Row(
+                        children: [
+                          Icon(Icons.star, color: Colors.amber, size: 24),
+                          SizedBox(width: 4),
+                          Text('${args.rating}',
+                              style: TextStyle(fontSize: 16)),
+                          SizedBox(width: 4),
+                          Text('(1,205)', style: TextStyle(color: Colors.grey)),
+                          Spacer(),
+                          Text('See all reviews',
+                              style: TextStyle(color: Colors.blue)),
+                        ],
                       ),
-                      child: Text('Add to Basket'),
-                    ),
-                  ],
+                      SizedBox(height: 16),
+                      Text(
+                        'A delicious chicken burger served on a toasted bun with fresh lettuce, tomato slices, and mayonnaise. Juicy grilled chicken patty seasoned to perfection.',
+                        style: TextStyle(fontSize: 16, color: Colors.grey),
+                      ),
+                      SizedBox(height: 16),
+                      Text(
+                        'Additional Options:',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 8),
+                      Column(
+                        children: [
+                          ListView.builder(
+                            shrinkWrap: true, // Adjust size based on content
+                            physics:
+                                NeverScrollableScrollPhysics(), // Disable internal scrolling
+                            itemCount: args.options.length,
+                            itemBuilder: (context, index) {
+                              return OptionItem(
+                                title: args.options[index].name,
+                                price: '+ £${args.options[index].price}',
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 16),
+                      Row(
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.remove),
+                            onPressed: () {},
+                          ),
+                          Text('1', style: TextStyle(fontSize: 18)),
+                          IconButton(
+                            icon: Icon(Icons.add),
+                            onPressed: () {},
+                          ),
+                          Spacer(),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 50, vertical: 12),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              backgroundColor: Colors.orange,
+                            ),
+                            child: Text('Add to Basket'),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
